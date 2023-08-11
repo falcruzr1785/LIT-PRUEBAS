a90541fda1e266de2907701b314a71d1ce108669
alcruz <falcruzr@gmail.com>
2023-08-11 15:59:12 -0600

    se quita lit y se agrega un div por MOD

--- componentes/lit-elements.js
+++ componentes/lit-elements.js
@@ -1,17 +1,20 @@
  
-import { LitElement, html, css } from 'lit';
-//lit basico
-export class LitElements extends LitElement {
-    static styles = [
-        css`
-            :host {
-                display: block;
-            }
-        `
-    ];
+// import { LitElement, html, css } from 'lit';
+// //lit basico
+// export class LitElements extends LitElement {
+//     static styles = [
+//         css`
+//             :host {
+//                 display: block;
+//             }
+//         `
+//     ];
 
-    render() {
-        return html`<h1> hola </h1>`;
-    }
-}
-customElements.define('lit-elements', LitElements);
+//     render() {
+//         return html`<h1> hola </h1>`;
+//     }
+// }
+// customElements.define('lit-elements', LitElements);
+
+const doc = document.getElementById("doc");
+doc.innerHTML = "<h1> Hola </h1>´";
